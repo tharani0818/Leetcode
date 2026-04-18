0343f9662f78e54b42559965d5a14448cbb800f7
tharani0818 <tharanijt_bit27@mepcoeng.ac.in>
2026-04-18 19:17:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>h=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!h.add(nums[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
