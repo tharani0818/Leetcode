7238e30c342db83c89981c55b90952a96361dc91
tharani0818 <tharanijt_bit27@mepcoeng.ac.in>
2026-04-18 19:28:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSegments(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)!=' ' && (i==0 || s.charAt(i-1)==' '))
+            {
+                c=c+1;
+            }
+        }
+        return c;
+    }
+}
