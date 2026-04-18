9321394353567db5b065a589dfe42ce230f1225a
tharani0818 <tharanijt_bit27@mepcoeng.ac.in>
2026-04-18 19:23:21 +0530

    Time: 14 ms (94.2%), Space: 51.5 MB (41.94%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length;
+        int m=s.length;
+        int l=0,r=0;
+        while(l<m && r<n)
+        {
+            if(g[r]<=s[l])
+            {
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+}
