88cf15ef041c58963757e1f6f5ff6c0ef0c42bc0
tharani0818 <tharanijt_bit27@mepcoeng.ac.in>
2026-04-18 19:51:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=3)
+        {
+            return n;
+        }
+        int a=2,b=3,c=0;
+        for(int i=3;i<n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
