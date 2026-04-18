a1c269deb0e4f9479d4ef4c0b0beca382b727f3a
tharani0818 <tharanijt_bit27@mepcoeng.ac.in>
2026-04-18 12:11:39 +0530

    Time: 1 ms (89.08%), Space: 43.1 MB (82.69%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int c=0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(word.charAt(i)>='A' && word.charAt(i)<='Z')
+            {
+                c=c+1;
+            }
+        }
+        if(c==word.length())
+        {
+            return true;
+        }
+        if(c==0)
+        {
+            return true;
+        }
+        if(c==1 && word.charAt(0)>='A'&&word.charAt(0)<='Z')
+        {
+            return true;
+        }
+        return false;
+    }
+}
