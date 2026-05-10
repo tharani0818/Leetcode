e587d5952863315ab47b3978058d36e72abb49bb
tharani0818 <tharanijt_bit27@mepcoeng.ac.in>
2026-05-10 11:59:42 +0530

    Time: 14 ms (80.95%), Space: 47.2 MB (15.48%) - LeetHub

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, -1);
+
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+
+                long diff = (long) nums[i] - nums[j];
+
+                if (diff >= -target && diff <= target && dp[j] != -1) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
